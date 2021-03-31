9feec73cd3e2017a7131d0ad26c284044dd8c5e5
neel244 <63952328+neel244@users.noreply.github.com>
2021-04-01 05:58:43 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+let daysItem = document.querySelector("#days");
+let hoursItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec");
+
+
+let countDown = () => {
+  let futureDate = new Date("19 April 2021");
+  let currentDate = new Date();
+  let myDate = futureDate - currentDate;
+  //console.log(myDate);
+
+  let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+
+  let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+
+  let min = Math.floor(myDate / 1000 / 60) % 60;
+
+  let sec = Math.floor(myDate / 1000) % 60;
+
+  daysItem.innerHTML = days;
+  hoursItem.innerHTML = hours;
+  minItem.innerHTML = min;
+  secItem.innerHTML = sec;
+
+  // if(sec == 0){
+  //   location.href = 'index.html';
+  // }
+}
+
+countDown()
+
+setInterval(countDown, 1000)
+
